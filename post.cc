cfb617e76df939580bfe13506e9c0f20742f01d9
Jörgen Grahn <grahn+src@snipabacken.se>
2018-12-02 10:33:10 +0100

    weather 2.1

--- post.cc
+++ post.cc
@@ -95,7 +95,7 @@ std::string post::req(const std::string& host,
     std::ostringstream req;
     req << "POST /v1.3/data.xml HTTP/1.1" << crlf
 	<< "Host: " << host << crlf
-	<< "User-Agent: weather/2.0" << crlf
+	<< "User-Agent: weather/2.1" << crlf
 	<< "Accept: */*" << crlf
 	<< "Content-Type: application/xml" << crlf
 	<< "Content-Length: " << bodys.size() << crlf
--- weather.cc
+++ weather.cc
@@ -273,7 +273,7 @@ int main(int argc, char ** argv)
 	    return 0;
 	    break;
 	case 'V':
-	    std::cout << "Weather 2.0\n"
+	    std::cout << "Weather 2.1\n"
 		      << "Copyright (c) 2018 Jörgen Grahn\n";
 	    return 0;
 	    break;
