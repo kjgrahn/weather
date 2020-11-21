01da8fb026ece0471587eb80c3a4572e24c83114
Jörgen Grahn <grahn+src@snipabacken.se>
2020-11-21 08:09:46 +0100

    weather 2.8

--- weather_week.cc
+++ weather_week.cc
@@ -131,8 +131,8 @@ int main(int argc, char ** argv)
 	    return 0;
 	    break;
 	case 'V':
-	    std::cout << "weather_week, part of Weather 2.7\n"
-		      << "Copyright (c) 2018, 2019 Jörgen Grahn\n";
+	    std::cout << "weather_week, part of Weather 2.8\n"
+		      << "Copyright (c) 2018, 2019, 2020 Jörgen Grahn\n";
 	    return 0;
 	    break;
 	case ':':
